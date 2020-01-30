a45f349d61c1428df349e563a50bc4106957c1ea
hpstream <915414300@qq.com>
2020-01-30 17:55:13 +0800

    fix:增加递归组件和全局组件案例

---
+++ src/components/notice.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text" v-if="isShow">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      isShow: false
+    };
+  },
+  methods: {
+    show() {
+      this.isShow = true;
+      // setTimeout(() => {
+      //   this.hide();
+      // }, 2000);
+    },
+    hide() {
+      this.isShow = false;
+      // this.$nextTick(() => {
+      //   this.$destroy();
+      // });
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+//@import url(); 引入公共css类
+.text {
+  position: fixed;
+  top: 10%;
+  left: 45%;
+  color: red;
+}
+</style>
---
+++ src/components/tree/item.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li>{{ treename }}</li>
+    <li v-if="children && children.length > 0">
+      <item
+        v-for="(item, index) of children"
+        :treename="item.name"
+        :key="index"
+        :children="item.children"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  props: {
+    treename: {
+      type: String,
+      default: ""
+    },
+    children: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    // console.log("------------------------------------");
+    // console.log(this.name);
+    // console.log("------------------------------------");
+  }
+};
+</script>
+<style lang="less" scoped>
+//@import url(); 引入公共css类
+li {
+  text-align: left;
+}
+</style>
